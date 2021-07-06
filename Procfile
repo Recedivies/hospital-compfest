b513320f3d349f30f2c2c6ac999cd553e66de6f8
recedivies <ahmadhiprananta@gmail.com>
2021-07-06 17:35:39 +0700

    final

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate --no-input
-web: gunicorn core.wsgi
+web: gunicorn hospital.wsgi --log-file -
--- hospital/settings.py
+++ hospital/settings.py
@@ -125,13 +125,13 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
